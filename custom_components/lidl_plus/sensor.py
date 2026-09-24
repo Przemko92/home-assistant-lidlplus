bcaa51aacbc70f2e656ae1957ed413c3da7f94a1
asd <asdsa>
2026-09-24 10:59:55 +0200

    chnaged class to total

--- custom_components/lidl_plus/sensor.py
+++ custom_components/lidl_plus/sensor.py
@@ -146,7 +146,7 @@ class LidlPlusTotalSpendSensor(LidlPlusEntity, SensorEntity):
 
     _attr_translation_key = "total_spend"
     _attr_device_class = SensorDeviceClass.MONETARY
-    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+    _attr_state_class = SensorStateClass.TOTAL
     _attr_suggested_display_precision = 2
 
     def __init__(self, coordinator: LidlPlusCoordinator) -> None:
